68648b70268ce22e20843d3b49e113f892b68089
Andy Truong <thehongtt@gmail.com>
2020-03-26 13:19:59 +1000

    DD tracer: Correct service name.

--- input-rabbitmq.go
+++ input-rabbitmq.go
@@ -58,7 +58,7 @@ func (this *RabbitMqInput) onMessage(ctx context.Context, m amqp.Delivery, handl
 	carrier := HeaderToTextMapCarrier(m.Headers)
 	if spanCtx, err := tracer.Extract(carrier); err == nil {
 		opts := []ddtrace.StartSpanOption{
-			tracer.ServiceName("explore-indexer"),
+			tracer.ServiceName("es-writer"),
 			tracer.SpanType("rabbitmq-consumer"),
 			tracer.ResourceName("consumer"),
 			tracer.Tag("message.routingKey", m.RoutingKey),
